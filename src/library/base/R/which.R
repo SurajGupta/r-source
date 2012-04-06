ccf05500193f83f870b1118a6e215e874051f68e
Suraj Gupta <surajg@gmail.com>
2012-04-06 16:08:21 -0400

    adding v0.63.0

--- src/library/base/R/which.R
+++
@@ -1,6 +0,0 @@
-## should return  integer :
-which <- function(x) {
-	if(is.logical(x))
-          if((n <- length(x))) (1:n)[x] else integer(0)
-	else stop("argument to \"which\" is not logical")
-}
